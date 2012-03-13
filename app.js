5bd56a408309fdd42abdd4123762bce778e00da9
Ju-hyung Lee <jufoot@gmail.com>
2012-03-13 13:23:02 +0900

    Minor fix.

--- app.js
+++ app.js
@@ -1,147 +1,136 @@
 App = function() {
-    var canvas;
-    var ctx;
-    var method = 0;
+	var canvas;
+	var ctx;
+	var method = 0;
 
-    // Spring Coefficients
-    var m = 1;
-    var L = 10;
-    var w = 2 * Math.PI * 8;
+	// Spring Coefficients
+	var m = 1;
+	var L = 10;
+	var w = 2 * Math.PI * 8;
+	var t0Length = 80;
 
-    function main() {
-        canvas = document.getElementById("canvas");
-        if (!canvas.getContext) {
-            alert("Couldn't get canvas object !");
-        }
+	function main() {
+		canvas = document.getElementById("canvas");
+		if (!canvas.getContext) {
+			alert("Couldn't get canvas object !");
+		}
 
-        // Main canvas context
-        ctx = canvas.getContext("2d");
+		// Main canvas context
+		ctx = canvas.getContext("2d");
 
-        // Transform coordinate system to y-axis is up
-        ctx.translate(0, canvas.height * 0.5);
-        ctx.scale(1, -1);        
+		// Transform coordinate system to y-axis is up
+		ctx.translate(0, canvas.height * 0.5);
+		ctx.scale(1, -1);        
 
-        updateScreen();
+		updateScreen();
 
-        document.getElementById("frequency").value = w / (2 * Math.PI);
-    }
+		document.getElementById("frequency").value = w / (2 * Math.PI);
+	}
 
-    function updateScreen() {
-        // Time interval
-        var h = 1 / 800;
+	function updateScreen() {		
+		// Time interval
+		var h = 1 / 800;
 
-        // Initial values
-        var f = { x: 100, v: 0 };        
-        if (method == 3) { // verlet
-            integrateRK4(f, h);
-            f.x = 100;
-            f.v = f.x; // previous position
-        }
+		// Select function of integration method
+		var integrate = [integrateExplicit, integrateMidPoint, integrateRK4, integrateImplicit, integrateSemiImplicit, integrateVerlet][method];
 
-        ctx.save();
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.restore();
+		// Initial values
+		var f = { x: t0Length, v: 0 };        
+		if (method == 5) { // verlet
+			integrateRK4(f, h);
+			f.v = f.x; // previous position
+		}		
 
-        ctx.beginPath();
+		// Integrate it for the first time
+		integrate(f, h);
 
-        integrate(f, h);
+		ctx.save();
+		ctx.setTransform(1, 0, 0, 1, 0, 0);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.restore();
 
-        ctx.moveTo(0, f.x);
+		ctx.beginPath();		
 
-        for (var x = 1; x < 800; x++) {
-            integrate(f, h);
-            ctx.lineTo(x, Math.max(Math.min(f.x, 800), -800));
-        }
-        
-        ctx.strokeStyle = "#000";
-        ctx.stroke();
-    }
+		ctx.moveTo(0, f.x);
 
-    function onchanged_method(value) {
-        method = value;
-        updateScreen();
-    }
+		for (var x = 1; x < 800; x++) {
+			integrate(f, h);
+			ctx.lineTo(x, Math.max(Math.min(f.x, 800), -800));
+		}
+		
+		ctx.strokeStyle = "#000";
+		ctx.stroke();
+	}
 
-    function onchanged_frequency(hz) {
-        w = 2 * Math.PI * hz;
-        updateScreen();
+	function onchanged_method(value) {
+		method = value;
+		updateScreen();
+	}
 
-        document.getElementById("frequency").value = hz;
-    }
+	function onchanged_frequency(hz) {
+		w = 2 * Math.PI * hz;
+		updateScreen();
 
-    function integrate(f, h) {
-        switch (method) {
-        case 0: integrateExplicit(f, h);
-            break;
-        case 1: integrateMidPoint(f, h);
-            break;
-        case 2: integrateRK4(f, h);
-            break;
-        case 3: integrateVerlet(f, h);
-            break;
-        case 4: integrateSemiImplicit(f, h);        
-            break;
-        case 5: integrateImplicit(f, h);
-            break;
-        }
-    }
+		document.getElementById("frequency").value = hz;
+	}
 
-    function integrateExplicit(f, h) {
-        var a = -w * w * (f.x - L);
-        f.x = f.x + f.v * h;
-        f.v = f.v + a * h;
-    }
+	function integrateExplicit(f, h) {
+		var a = -w * w * (f.x - L);
+		f.x = f.x + f.v * h;
+		f.v = f.v + a * h;
+	}
 
-    function integrateMidPoint(f, h) {
-        var a = -w * w * (f.x - L);
-        v_half = f.v + a * h * 0.5;
-        f.x = f.x + v_half * h;
-        a = -w * w * (f.x - L);
-        f.v = f.v + a * h;
-    }
+	function integrateMidPoint(f, h) {
+		var a = -w * w * (f.x - L);
+		v_half = f.v + a * h * 0.5;
+		f.x = f.x + v_half * h;
+		a = -w * w * (f.x - L);
+		f.v = f.v + a * h;
+	}
 
-    function integrateRK4(f, h) {
-        var a1 = -w * w * (f.x - L);
-        var v1 = f.v;
+	function integrateRK4(f, h) {
+		var a1 = -w * w * (f.x - L);
+		var v1 = f.v;
 
-        var a2 = -w * w * (f.x + v1 * h * 0.5 - L);
-        var v2 = f.v + a1 * h * 0.5;
+		var a2 = -w * w * (f.x + v1 * h * 0.5 - L);
+		var v2 = f.v + a1 * h * 0.5;
 
-        var a3 = -w * w * (f.x + v2 * h * 0.5 - L);
-        var v3 = f.v + a2 * h * 0.5;
-        
-        var a4 = -w * w * (f.x + v3 * h - L);
-        var v4 = f.v + a3 * h;
+		var a3 = -w * w * (f.x + v2 * h * 0.5 - L);
+		var v3 = f.v + a2 * h * 0.5;
+		
+		var a4 = -w * w * (f.x + v3 * h - L);
+		var v4 = f.v + a3 * h;
 
-        var v = (v1 + 2 * v2 + 2 * v3 + v4) / 6;
-        var a = (a1 + 2 * a2 + 2 * a3 + a4) / 6;
+		var v = (v1 + 2 * v2 + 2 * v3 + v4) / 6;
+		var a = (a1 + 2 * a2 + 2 * a3 + a4) / 6;
 
-        f.x = f.x + v * h;
-        f.v = f.v + a * h;
-    }
+		f.x = f.x + v * h;
+		f.v = f.v + a * h;
+	}
 
-    function integrateVerlet(f, h) {
-        var a = -w * w * (f.x - L);
-        var x = 2 * f.x - f.v + h * h * a;
-        f.v = f.x; // This is a previous position not a velocity
-        f.x = x;
-    }
+	 function integrateImplicit(f, h) {     
+		var wwh = w * w * h;
+		f.v = (f.v - wwh * f.x + wwh * L) / (1 + wwh * h);
+		f.x = f.x + f.v * h;
+	}
 
-    function integrateSemiImplicit(f, h) {
-        var a = -w * w * (f.x - L);
-        f.v = f.v + a * h;
-        f.x = f.x + f.v * h;
-    }
+	function integrateSemiImplicit(f, h) {
+		var a = -w * w * (f.x - L);
+		f.v = f.v + a * h;
+		f.x = f.x + f.v * h;
+	}
 
-    function integrateImplicit(f, h) {     
-        var wwh = w * w * h;
-        f.v = (f.v - wwh * f.x + wwh * L) / (1 + wwh * h);
-        f.x = f.x + f.v * h;
-    }
+	function integrateVerlet(f, h) {
+		var a = -w * w * (f.x - L);
+		var x = 2 * f.x - f.v + h * h * a;
+		f.v = f.x; // This is a previous position not a velocity
+		f.x = x;
+	}
 
-    return { main: main, 
-        onchanged_method: onchanged_method,
-        onchanged_frequency: onchanged_frequency
-    };
-} ();
+	return { main: main, 
+		onchanged_method: onchanged_method,
+		onchanged_frequency: onchanged_frequency
+	};
+}();
+
+window.addEventListener("load", App.main, false);
